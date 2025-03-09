2fe6bbf4529f2c941c04e4885c9a011fc8b839a5
Swiatlon <przemyslaw.swiatlon@gmail.com>
2025-03-09 13:07:19 +0100

    QF

--- src/redux/api/today-quests-api.ts
+++ src/redux/api/today-quests-api.ts
@@ -1,12 +1,6 @@
 import Api from '../config/api';
 import { AllQuestsUnion } from '@/hooks/quests/useGetAllQuests';
 
-interface ApiResponseItem {
-  id: number;
-  questType: string;
-  quest: AllQuestsUnion;
-}
-
 export const todayQuestSlice = Api.injectEndpoints({
   endpoints: builder => ({
     getAllTodayQuests: builder.query<AllQuestsUnion[], void>({
@@ -14,10 +8,6 @@ export const todayQuestSlice = Api.injectEndpoints({
         method: 'GET',
         url: '/all-quests',
       }),
-      // transformResponse: (response: ApiResponseItem[]) =>
-      //   response.map(({ quest }) => ({
-      //     ...quest,
-      //   })),
       providesTags: ['todayQuestsGet'],
     }),
   }),
