806e945690008883f23eff4dbd35941873066835
Swiatlon <przemyslaw.swiatlon@gmail.com>
2025-03-22 22:05:03 +0100

    Added removing validation on same value

--- src/components/views/quests/tags/tag-modals/schema.ts
+++ src/components/views/quests/tags/tag-modals/schema.ts
@@ -1,23 +1,21 @@
 import * as yup from 'yup';
 import { IQuestLabel } from '@/contract/quests/labels/labels-quests';
 
-export const tagValidationSchema = (questsLabels: IQuestLabel[]) => {
-  const tagValidationSchema = yup.object().shape({
+export const tagValidationSchema = (questsLabels: IQuestLabel[], previousValue?: string) => {
+  return yup.object().shape({
     value: yup
       .string()
       .trim()
       .required('Tag cannot be empty')
       .max(25, 'Tag is too long. Please keep it under 25 characters.')
       .test('unique-tag', 'This tag name already exists. Please choose another.', value => {
-        if (value) {
-          return questsLabels.every(label => label.value.toLowerCase() !== value.toLowerCase());
+        if (previousValue && value.toLowerCase() === previousValue.toLowerCase()) {
+          return true;
         }
 
-        return true;
+        return questsLabels.every(label => label.value.toLowerCase() !== value.toLowerCase());
       }),
     backgroundColor: yup.string().default('#1987EE'),
     textColor: yup.string().default('#FFFFFF'),
   });
-
-  return tagValidationSchema;
 };
