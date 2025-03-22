c878e52f3cf6a8cf85724bbd7e19d6b3285cb93c
Swiatlon <przemyslaw.swiatlon@gmail.com>
2025-03-22 21:14:06 +0100

    Monthly format showing

--- src/components/views/quests/reusable/quest-item/quest-item-date-monthly.tsx
+++ src/components/views/quests/reusable/quest-item/quest-item-date-monthly.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 interface QuestItemDateMonthlyProps {
   startDay: number;
   endDay: number;
 }
 
-const QuestItemDateMonthly: React.FC<QuestItemDateMonthlyProps> = ({ startDay, endDay }) => {
-  if (startDay === endDay) {
-    return <Text className="text-sm text-gray-500">Repeats monthly every: {startDay!.toString().padStart(2, '0')}</Text>;
-  }
+const CircleNumber = ({ text }: { text: string }) => (
+  <View className="ml-1 px-3 py-1.5 bg-blue-500 rounded-full flex items-center justify-center">
+    <Text className="text-white font-bold text-xs">{text}</Text>
+  </View>
+);
 
+const QuestItemDateMonthly: React.FC<QuestItemDateMonthlyProps> = ({ startDay, endDay }) => {
   return (
-    <Text className="text-sm text-gray-500">
-      Repeats monthly bettwen: {startDay!.toString().padStart(2, '0')}-{endDay!.toString().padStart(2, '0')}
-    </Text>
+    <View className="flex-row items-center gap-1">
+      <Text className="text-sm text-gray-500">Repeats monthly:</Text>
+      <CircleNumber
+        text={
+          startDay === endDay
+            ? startDay.toString().padStart(2, '0')
+            : `${startDay.toString().padStart(2, '0')} - ${endDay.toString().padStart(2, '0')}`
+        }
+      />
+    </View>
   );
 };
+
 export default QuestItemDateMonthly;
