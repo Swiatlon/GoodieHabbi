03cca91c43d2fb62de4ba21d9dcfc38c1988ac97
Swiatlon <przemyslaw.swiatlon@gmail.com>
2025-03-22 22:12:23 +0100

    Changed icon for dashboard

--- src/components/shared/drawer/routes-configs/dashboard-config.tsx
+++ src/components/shared/drawer/routes-configs/dashboard-config.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 import CustomDrawerItem from '../elements/custom-drawer-item';
 
 const DashboardConfig = () => {
-  return <CustomDrawerItem label="Dashboard" icon={<AntDesign name="dashboard" />} route="(authorized)/dashboard" />;
+  return <CustomDrawerItem label="Dashboard" icon={<Ionicons name="grid-outline" size={24} />} route="(authorized)/dashboard" />;
 };
 
 export default DashboardConfig;
