d0ffb82e910ba46862c3479eacd1d67f0a62f613
Swiatlon <przemyslaw.swiatlon@gmail.com>
2025-05-11 20:12:37 +0200

    Cented content and added navigation on logo

--- src/components/shared/app-bar/app-bar.tsx
+++ src/components/shared/app-bar/app-bar.tsx
@@ -21,24 +21,37 @@ const AppBar: React.FC = () => {
     navigation.navigate('(authorized)/profile');
   };
 
+  const handleLogoClick = () => {
+    if (isAuthenticated) {
+      navigation.navigate('(authorized)/dashboard');
+      return;
+    }
+
+    navigation.navigate('(not-authorized)/login');
+  };
+
+  const handleMenuClick = () => {
+    navigation.toggleDrawer();
+  };
+
   return (
     <View className="bg-primary flex flex-row justify-between items-center px-4 py-2">
       <Animated.View style={animatedMenuStyle}>
-        <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+        <TouchableOpacity onPress={handleMenuClick}>
           <Ionicons name="menu" size={28} color="white" className="w-[50px]" />
         </TouchableOpacity>
       </Animated.View>
       <Animated.View style={animatedLogoStyle}>
-        <Image source={logoHeader} style={{ width: 50, height: 50 }} resizeMode="contain" />
+        <TouchableOpacity onPress={handleLogoClick}>
+          <Image source={logoHeader} style={{ width: 50, height: 50 }} resizeMode="contain" />
+        </TouchableOpacity>
       </Animated.View>
-      <View>
-        {isAuthenticated ? (
-          <Animated.View style={animatedProfileStyle}>
-            <TouchableOpacity onPress={handleProfileClick} style={animatedProfileStyle}>
-              <Image source={exampleUserIcon} style={{ width: 50, height: 50 }} resizeMode="contain" />
-            </TouchableOpacity>
-          </Animated.View>
-        ) : null}
+      <View className={`${isAuthenticated ? 'opacity-100' : 'opacity-0'}`}>
+        <Animated.View style={animatedProfileStyle}>
+          <TouchableOpacity onPress={handleProfileClick} style={animatedProfileStyle}>
+            <Image source={exampleUserIcon} style={{ width: 50, height: 50 }} resizeMode="contain" />
+          </TouchableOpacity>
+        </Animated.View>
       </View>
     </View>
   );
