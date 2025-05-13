253591fd016eb46139d62dde69e84997cabf0dfc
Swiatlon <przemyslaw.swiatlon@gmail.com>
2025-05-13 21:04:57 +0200

    failed quest icon

--- src/components/views/quests/reusable/quest-item/quest-item-date.tsx
+++ src/components/views/quests/reusable/quest-item/quest-item-date.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import dayjs from '@/configs/day-js-config';
 import { NullableString, UndefinedString } from '@/types/global-types';
 import { safeDateFormat } from '@/utils/utils';
@@ -39,11 +40,10 @@ const QuestItemDate: React.FC<QuestItemDateProps> = ({ startDate, endDate }) => 
   const daysLeftColor = daysLeft <= 5 ? 'text-error' : daysLeft > 5 && daysLeft <= 10 ? 'text-warning' : 'text-gray-500';
 
   return (
-    <View>
-      <Text className="text-sm text-gray-400">
-        {dateLabel}
-        {endDate && daysLeft >= 0 && <Text className={`font-bold ${daysLeftColor}`}>({daysLeft} days left)</Text>}
-      </Text>
+    <View className="flex-row items-center gap-1">
+      <Text className="text-sm text-gray-400">{dateLabel}</Text>
+      {endDate && daysLeft >= 0 && <Text className={`text-sm font-bold ${daysLeftColor}`}>({daysLeft} days left)</Text>}
+      {endDate && daysLeft < 0 && <MaterialIcons color="red" size={14} name="alarm-off" />}
     </View>
   );
 };
