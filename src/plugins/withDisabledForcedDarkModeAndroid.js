343655bfda534a647d885a489e2904734ccb63dc
Swiatlon <przemyslaw.swiatlon@gmail.com>
2026-09-20 19:41:06 +0200

    FIX

--- src/plugins/withDisabledForcedDarkModeAndroid.js
+++ src/plugins/withDisabledForcedDarkModeAndroid.js
@@ -3,7 +3,7 @@ const { createRunOncePlugin, withAndroidStyles, AndroidConfig } = require('expo/
 function setForceDarkModeToFalse(styles) {
   styles = AndroidConfig.Styles.assignStylesValue(styles, {
     add: true,
-    parent: AndroidConfig.Styles.getAppThemeLightNoActionBarGroup(),
+    parent: AndroidConfig.Styles.getAppThemeGroup(),
     name: `android:forceDarkAllowed`,
     value: 'false',
   });
